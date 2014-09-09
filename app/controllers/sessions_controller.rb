b12ef508ea45a2b3d942033c6fee3b61c72675d1
wuzicheng <sonhit@163.com>
2014-09-09 17:41:11 +0800

    initial creation

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user&&user.authenticate(params[:session][:password])
+			sign_in user
+			if user.usertype =="A"
+				redirect_to user
+			elsif user.usertype == "S"
+				redirect_to requirements_path
+			else user.usertype == "P"
+				redirect_to products_path
+			end
+		else
+			#create error message
+			flash.now[:error]="错误的用户名或者密码"
+			render 'new'
+		end
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_path
+	end
+end
